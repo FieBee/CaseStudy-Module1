8762522812075999d6e0bd0f78c4977df9510b7a
FieBee <phuonglf98@gmail.com>
2022-06-27 17:16:37 +0700

    them va cham vien canvas

--- Js/Impact.js
+++ Js/Impact.js
@@ -1,8 +1,8 @@
 function Impact(){
-    if (ball1.x - ball1.radius <=0 || ball1.radius + ball1.x >= canvas.offsetWidth){
+    if (ball1.x - ball1.radius <= 3 || ball1.radius + ball1.x >= canvas.offsetWidth-3){
         dx = -dx
     }
-    else if (ball1.y - ball1.radius <= 0){
+    else if (ball1.y - ball1.radius <= 3){
         dy = -dy
         score ++
         if (score === 2){
@@ -27,7 +27,7 @@ function Impact(){
             ball1.color =  "#ff3030"
         }
     }
-    else if(ball1.y + ball1.radius >= canvas.offsetHeight){
+    else if(ball1.y + ball1.radius >= canvas.offsetHeight-5){
         if(ball1.x  > paddle1.x && ball1.x < paddle1.x + paddle1.width){
             dy = -dy
         }
@@ -36,6 +36,7 @@ function Impact(){
             alert("Your score: "+score)
             stop()
             displayScore(score)
+            document.getElementById("start").style.display = "none"
             document.getElementById("showScore").innerHTML = str2
             str1 =""
         }
